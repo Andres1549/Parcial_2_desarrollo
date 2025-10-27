0b9520ec56d4b7b2c7109e5342058543e08ba9d4
Andres <afbasto49@ucatolica.edu.co>
2025-10-27 08:57:08 -0500

    requirements, db,py y inicio de las declaraciones

---
+++ app/db.py
@@ -0,0 +1,14 @@
+from sqlmodel import create_engine, Session, SQLModel
+from typing import Generator
+
+sqlite_file_name = "app/database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=False, connect_args={"check_same_thread": False})
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
